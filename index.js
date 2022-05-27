38ebecdcecc726c9b1ca96d94dcc407ab3a897a3
nishattasnim08 <nishattasnim24258@gmail.com>
2022-05-27 22:47:15 +0600

    database connected

--- index.js
+++ index.js
@@ -9,6 +9,31 @@ app.use(express.json())
 
 const port = process.env.PORT || 5000;
 
+
+
+
+
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.mkuxsxx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+ console.log('CONNECTED');
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
+
+
+
+
+
+
 app.get("/", (req, res) => {
   res.send("Cooky is Cooking!");
 });
