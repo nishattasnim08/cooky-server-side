3d975f00b7a3241f284e21efc60f54235331e4e5
nishattasnim08 <nishattasnim24258@gmail.com>
2022-05-27 23:11:14 +0600

    get data from server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 require('dotenv').config()
 var cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -17,14 +18,29 @@ const port = process.env.PORT || 5000;
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.mkuxsxx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
- console.log('CONNECTED');
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const itemCollection = client.db("cookydb").collection("items");
+        
+        app.get("/items", async (req, res) => {
+            const query = {};
+            const cursor = await itemCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+          });
+    
+
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 
@@ -35,9 +51,9 @@ client.connect(err => {
 
 
 app.get("/", (req, res) => {
-  res.send("Cooky is Cooking!");
+    res.send("Cooky is Cooking!");
 });
 
 app.listen(port, () => {
-  console.log("cooking");
+    console.log("cooking");
 });
