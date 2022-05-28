60fde3cb1b018093915d7f5fe20a71e5b6040999
nishattasnim08 <nishattasnim24258@gmail.com>
2022-05-28 20:41:28 +0600

    review data fetched

--- index.js
+++ index.js
@@ -36,6 +36,13 @@ async function run() {
             res.send(result);
         });
 
+        app.get("/review", async (req, res) => {
+            const query = {};
+            const cursor = await reviewCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
 
         app.get("/item/:id", async (req, res) => {
             const id = req.params.id;
